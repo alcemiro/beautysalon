8ce0851ec8bb9ee45c6a46b855e22ac02e1aa99d
alcemiro <64944984+alcemiro@users.noreply.github.com>
2021-09-16 09:10:27 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,116 @@
+//abre fecha menu ao clicar icone: hamburguer
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle)
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+
+// quando clicar em um itemm do menu
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/***mudar o header da pagina quando der scrool** */
+
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
+  if (window.scrollY >= navHeight) {
+    //scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/*****  Testimonials ****/
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  Keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWraperSize: true
+    }
+  }
+})
+
+/****** Scrollreveal mostra elementos quando der scroll na pagina ***** */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .text, #home .image, 
+   #about .image, #about .text,
+   #services header, #services .card,
+   #testimonials header, #testimonials .testimonials
+   #contact .links, #contact .text,
+   footer .brand, footer .social 
+   `,
+
+  { interval: 100 }
+)
+
+/*** botão voltar para o topo ** */
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/*** menu ativo conforme a secção visivel na pagina**/
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection () {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for ( const section of sections ) {
+    const sectionTop = section.offsetTop
+    const sectionHeigth = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeigth
+
+    if (checkpointStart && checkpointEnd) {
+      document
+      .querySelector ('nav ul li a [href*=' + sectionId + ']')
+      .classList.add('active')
+    }else {
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']' )
+      .classList.remove('active')
+    }
+  }
+    
+  }
+
+}
+
+
+/** When scroll * */
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
